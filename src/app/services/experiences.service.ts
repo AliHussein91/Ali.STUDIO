7ea2679c8fe4bbd53293f9a426a0da3dca7df56f
Ali Hussein <ali.hussein@live.com>
2024-09-10 19:55:26 +0300

    Minor updates

--- src/app/services/experiences.service.ts
+++ src/app/services/experiences.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Experience } from '../interfaces/experience';
 import { HttpClient } from '@angular/common/http';
-import { Project } from '../interfaces/project';
 import { Observable } from 'rxjs';
 
 @Injectable({
