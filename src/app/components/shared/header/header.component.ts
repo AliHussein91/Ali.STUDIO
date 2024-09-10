742ae5804bccc5768505ed17b7b20c6ecbe5302f
Ali Hussein <ali.hussein@live.com>
2024-09-11 02:33:01 +0300

    Updated products page navigation to close and return to home

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,14 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  Event,
+  NavigationEnd,
+  Params,
+} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,CommonModule],
+  imports: [RouterLink, CommonModule, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isProductPage!: boolean;
+  router: Router = inject(Router);
 
+  ngOnInit(): void {
+    this.router.events.subscribe((routerEvent: Event) => {
+      if (routerEvent instanceof NavigationEnd) {
+        if (this.router.url.split('/')[1] === 'projects') {
+          this.isProductPage = true;
+        } else {
+          this.isProductPage = false;
+        }
+      }
+    });
+  }
 }
--- src/app/services/experiences.service.ts
+++ src/app/services/experiences.service.ts
@@ -3,6 +3,7 @@ import { Experience } from '../interfaces/experience';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root',
 })
